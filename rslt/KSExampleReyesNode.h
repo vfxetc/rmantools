bd0e5daaac82f44b73b0c89481cacbe531907e11
Mike Boers <westernx@mikeboers.com>
2015-04-22 11:37:42 -0700

    Fix classification of KSExampleReyesNode

--- rslt/KSExampleReyesNode.h
+++ rslt/KSExampleReyesNode.h
@@ -6,11 +6,17 @@ slim 1 extensions pixar_db {
 extensions pixar rfm_ {
 
     template void KSExampleReyesNode {
-
+        
+        
+        # Classifications include:
+        #     - rendernode/RenderMan/texture/3d: RfM connects a new
+        #       place3dTexture to the "pm" attribute.
+        #     - rendernode/RenderMan/texture/2d: RfM likely does something,
+        #       but I haven't tried yet.
+        #     - rendernode/RenderMan/utility: does nothing special.
         userdata {
             rfm_nodeid 2000012
-            rfm_classification \
-                shader/surface:rendernode/RenderMan/shader/surface:swatch/rmanSwatch
+            rfm_classification rendernode/RenderMan/utility
         }
 
         parameter point location
