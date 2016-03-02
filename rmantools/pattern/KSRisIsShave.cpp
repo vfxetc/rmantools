7af6613a1e4101a23b061bbba842d2377d4624fc
Mike Boers <westernx@mikeboers.com>
2016-03-02 12:25:09 -0800

    First pass of KS*IsShave nodes

---
+++ rmantools/pattern/KSRisIsShave.cpp
@@ -0,0 +1,111 @@
+#include "RixPattern.h"
+#include "RixShadingUtils.h"
+#include "RixColorUtils.h"
+#include "RixIntegrator.h"
+#include <cstring>
+#include <stdint.h>
+#include <stdio.h>
+
+
+#define DEBUG(x, ...) printf("[KSRisIsShave] " x "\n", ##__VA_ARGS__); fflush(stdout);
+
+
+class KSRisIsShave : public RixPattern
+{
+public:
+
+    // Stubs.
+    KSRisIsShave() {}
+    virtual ~KSRisIsShave() {}
+    virtual int Init(RixContext &, char const *pluginpath) { return 0; }
+    virtual void Finalize(RixContext &) {}
+    
+    virtual RixSCParamInfo const *GetParamTable();
+    
+    virtual int ComputeOutputParams(RixShadingContext const *,
+                                    RtInt *n, RixPattern::OutputSpec **outputs,
+                                    RtConstPointer instanceData,
+                                    RixSCParamInfo const *);
+
+};
+
+
+
+// NOTE: This MUST match the order of parameters returned from GetParamTable().
+enum paramId
+{
+    k_result,
+    k_primvarName,
+    k_assumeTrue
+};
+
+
+RixSCParamInfo const *
+KSRisIsShave::GetParamTable()
+{
+    static RixSCParamInfo s_ptable[] =
+    {
+        RixSCParamInfo("result", k_RixSCFloat, k_RixSCOutput),
+        RixSCParamInfo("primvarName", k_RixSCString),
+        RixSCParamInfo("assumeTrue", k_RixSCFloat),
+        RixSCParamInfo(), // End of table.
+    };
+    return &s_ptable[0];
+}
+
+
+int
+KSRisIsShave::ComputeOutputParams(RixShadingContext const *sctx,
+                                RtInt *noutputs, OutputSpec **outputs,
+                                RtConstPointer instanceData,
+                                RixSCParamInfo const *ignored)
+{
+    
+    RixShadingContext::Allocator pool(sctx);
+    OutputSpec* out = pool.AllocForPattern<OutputSpec>(1);
+    *outputs = out;
+    *noutputs = 1;
+
+    out->paramId = k_result;
+    out->detail = k_RixSCUniform;
+    float *resultValue = (float*)pool.AllocForPattern<RtFloat>(1);
+    out->value = resultValue;
+    
+    // Grab the default variable.
+    // NOTE: This takes the first value if it is varying.
+    RtFloat const *assumeTrue;
+    if (sctx->EvalParam(k_assumeTrue, -1, &assumeTrue, NULL)) {
+        *resultValue = assumeTrue[0];
+    }
+    
+    // Grab the requested primvar name.
+    RtConstString *primvarName;
+    sctx->EvalParam(k_primvarName, -1, &primvarName, NULL);
+    if (!*primvarName || !strlen(*primvarName)) {
+        return 0; // Not set; bail!
+    }
+        
+    // DEBUG("primvarName = \"%s\"", *primvarName);
+    
+    // Check for presense.
+    RixSCType type;
+    RtInt arrayLen;
+    RixSCDetail detail = sctx->GetPrimVar(*primvarName, &type, &arrayLen);
+    if (detail) {
+        *resultValue = 1.f;
+    }
+    
+    return 0;
+}
+
+
+RIX_PATTERNCREATE
+{
+    return new KSRisIsShave();
+}
+
+RIX_PATTERNDESTROY
+{
+    delete ((KSRisIsShave*)pattern);
+}
+
---
+++ rmantools/rslt/KSReyesIsShave.h
@@ -0,0 +1,63 @@
+#ifndef KS_REYES_HAIR_FADE_H
+#define KS_REYES_HAIR_FADE_H
+/*
+<rman id="rslt">
+slim 1 extensions pixar_db {
+extensions pixar {} {
+
+    template void KSReyesIsShave {
+        
+        description {
+            Determine if the current surface is hair from Shave and a Haircut.
+        }
+
+        userdata {
+            rfm_nodeid 2000017
+            rfm_classification rendernode/RenderMan/utility
+        }
+        
+        parameter string primvarName {
+            detail cantvary
+            label "Primitive Variable"
+            description {
+                Which float primvar to test for; defaults to "SHAVEambdiff".
+            }
+            default "SHAVEambdiff"
+        }
+        
+        parameter float assumeTrue {
+            detail cantvary
+            subtype switch
+            description {
+                Output true.
+            }
+            default 0
+        }
+
+        parameter { output float } result { detail mustvary }
+
+        RSLInclude KSReyesIsShave.h
+        RSLFunction {
+            // No extra source for KSReyesIsShave.
+        }
+
+    }
+}
+}
+</rman>
+*/
+
+
+
+void KSReyesIsShave(
+    string primvarName;
+    float assumeTrue;
+    output float result;
+) {
+    float primvarValue; // Just for the signal.
+    result = assumeTrue != 0 ? 1 : readprimvar(primvarName, primvarValue);
+}
+
+
+
+#endif
